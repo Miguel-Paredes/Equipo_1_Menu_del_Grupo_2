2fc0d3f33edf97e2f133c493690f9863d1135dda
AzureAD\10800 <os.miguelparedes@gmail.com>
2023-06-19 16:28:10 -0500

    Menu 1

--- src/menu.java
+++ src/menu.java
@@ -1,5 +1,12 @@
+import conicas2.circunferencia_grupo2;
+import conicas2.Elipse;
+import conicas2.hiperbole;
+import conicas2.Parabola_Equipo2;
 public class menu {
     public static void main(String[] args) {
-
+    circunferencia_grupo2 circulo = new circunferencia_grupo2(2,3);
+    Elipse curva_plana = new Elipse(1,2);
+    hiperbole hiper = new hiperbole(2.1,3.3);
+    Parabola_Equipo2 parabola = new Parabola_Equipo2(5,6,1,2,3);
     }
 }
